2169523acd02abfccbd6ff45573db40382cd1d43
Ruth <ruro122020@gmail.com>
2024-02-29 02:23:41 -0500

    inital commit

---
+++ migrations/versions/1890c0c3fc0c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1890c0c3fc0c
+Revises: 
+Create Date: 2024-02-28 20:07:37.432928
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1890c0c3fc0c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
